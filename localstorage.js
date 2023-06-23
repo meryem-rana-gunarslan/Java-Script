8eac2c53fbe5894c403a20f065eba0e45798d1e9
Meryem Gunarslan <meryemranagunarslan@gmail.com>
2023-06-23 11:44:15 -0500

    Local Storage

--- localstorage.js
+++ localstorage.js
@@ -1,10 +1,43 @@
-//LOCAL STORAGE
+// //LOCAL STORAGE
+let val;
+
+//set item
+ 
+const firstName = localStorage.setItem('firstName', 'Meryem ');
+const lastName = localStorage.setItem('lastName', 'Gunarslan');
+let hobies = ['sinema', 'araba', 'gezmek'];
+
+//get item
+val = localStorage.getItem('firstName');
+val = localStorage.getItem('lastName');
+
+//remove item
+// localStorage.removeItem('firstName');
+// localStorage.removeItem('lastName');
+
+//clear
+//localStorage.clear();
+
+//set array to storage
+localStorage.setItem('hobies',JSON.stringify (hobies));
+
+val = JSON.parse(localStorage.getItem('hobies'));
+
+
+
+console.log(val)
 console.log(localStorage);
 
 
 
+
 //SESSION STORAGE
-console.log(sessionsStorage);
+// const city = sessionStorage.setItem('city', 'Baton Rouge');
+
+// const country = sessionStorage.setItem('country', 'USA');
+
+
+// console.log(sessionsStorage);
 
 
 
